b38c1ccd85e572a06eb7ab86c1cfc194deec3990
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-01-17 17:26:32 -0500

    fixed stars, broke colors

--- index.js
+++ index.js
@@ -38,36 +38,87 @@ inquirer
            followers: res.data.followers,
            following: res.data.following,
            blog: res.data.blog,
-           stars: res.data.starred_url
+           stars: res.data.starred
        };
+
+       let starCount;
+
+       const starUrl = `https://api.github.com/users/${username}/starred`;
+       axios.get(starUrl).then(res => {
+         let starCount = {
+           stars: res.data.starred
+         }
+         console.log(starCount)
+         console.log(res.data)
+         const starz = res.data.length
+         console.log(res.data.length)
+
+          const html = `<html>
+          <styleop = "background-color: ${colorPref}">
+          <img class = "center"
+          src =  "${userInfo.image}">
+          <p> Name: ${userInfo.name}</p>
+          <p> bio: ${userInfo.bio}</p>
+          <p> location: ${userInfo.location} </p>
+          <p> github: ${userInfo.github}</p>
+          <p> repos: ${userInfo.repos}</p>
+          <p> followers: ${userInfo.followers} </p>
+          <p> following: ${userInfo.following} </p>
+          <p> blog: ${userInfo.blog} </p>
+          <p> stars: ${res.data.length} </p>
+          </body>
+
+          </html>`
+
+          const options = {
+            "orientation": "portrait",
+            "type": "pdf"
+
+            
+        }
+
+        const colors = {
+          purple: {
+            wrapperBackground: "#C523EB",
+            headerBackground: "#C523EB",
+            headerColor: "black",
+            photoBorderColor: "#black"
+          },
+          orange: {
+              wrapperBackground: "orange",
+              headerBackground: "orange",
+              headerColor: "white",
+              photoBorderColor: "#FEE24C"
+            },
+          blue: {
+            wrapperBackground: "blue",
+            headerBackground: "#26175A",
+            headerColor: "white",
+            photoBorderColor: "#73448C"
+          },
+         
+          grey: {
+            wrapperBackground: "grey",
+            headerBackground: "grey",
+            headerColor: "white",
+            photoBorderColor: "white"
+          }
+        
+      }
+ 
+     
+ 
+        pdf.create(html, options).toFile('./profile.pdf', function(err, res) {
+         if (err) return console.log(err);
+         console.log(res); // { filename: '/app/businesscard.pdf' }
+       });
+       })
+       
+       
        console.log(userInfo, res.data)
 
-       const html = `<html>
-       <body style = "background-color: ${colorPref}">
-       <img class = "center"
-       src =  "${userInfo.image}">
-       <p> Name: ${userInfo.name}</p>
-       <p> bio: ${userInfo.bio}</p>
-       <p> location: ${userInfo.location} </p>
-       <p> github: ${userInfo.github}</p>
-       <p> repos: ${userInfo.repos}</p>
-       <p> followers: ${userInfo.followers} </p>
-       <p> following: ${userInfo.following} </p>
-       <p> blog: ${userInfo.blog} </p>
-       <p> stars: ${userInfo.stars} </p>
-       </body>
-       </html>`
-       const options = {
-           "orientation": "portrait",
-           "type": "pdf"
-       }
-
-    
-
-       pdf.create(html, options).toFile('./profile.pdf', function(err, res) {
-        if (err) return console.log(err);
-        console.log(res); // { filename: '/app/businesscard.pdf' }
-      });
+       
+       
 
      //  let user = generateHTML(userInfo)
 
@@ -88,32 +139,5 @@ inquirer
     //   console.log('Successfully created pdf!');
     //     console.log(res);
     })
-    const colors = {
-        purple: {
-          wrapperBackground: "#C523EB",
-          headerBackground: "#C523EB",
-          headerColor: "black",
-          photoBorderColor: "#black"
-        },
-        orange: {
-            wrapperBackground: "orange",
-            headerBackground: "orange",
-            headerColor: "white",
-            photoBorderColor: "#FEE24C"
-          },
-        blue: {
-          wrapperBackground: "blue",
-          headerBackground: "#26175A",
-          headerColor: "white",
-          photoBorderColor: "#73448C"
-        },
-       
-        grey: {
-          wrapperBackground: "grey",
-          headerBackground: "grey",
-          headerColor: "white",
-          photoBorderColor: "white"
-        }
-      
-    }
+    
 })
